9a7e660ee622f1535cf37ea527ee67dac1c057ab
Sai-kishan-K <saik77556@gmail.com>
2021-08-17 17:33:55 +0530

    mainFyle

---
+++ app.js
@@ -0,0 +1,14 @@
+// var contactbtn=document.getElementById("contactbtn")
+
+// if(contactbtn.addEventListener('click',openForm()))
+// {
+// function openForm(){
+//     document.getElementById("myForm").style.display="block"
+// }
+// }
+// else{
+//     document.getElementById("myForm").style.display="none"
+// }
+function openForm(){
+    window.location.replace("https://sai-kishan-k.github.io/fyle2/")
+}
